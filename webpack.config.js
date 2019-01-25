3b9b6fcdff1635b2669247e430775751e603eed0
skywalker512 <houzhenhong@hotmail.com>
2019-01-25 21:37:51 +0800

    [修改] 修改 webpack 入口文件
    使他可以支持多个入口文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
