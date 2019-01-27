665ebc939ca3e3e11f22994f615010c5122392dd
skywalker512 <houzhenhong@hotmail.com>
2019-01-27 21:32:08 +0800

    [提交] 添加前端 setting 用于设置 api 地址

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,9 +1,11 @@
+import { setting } from './setting';
+
 function getJSON(url, callback) {
   let xhr = new XMLHttpRequest();
   xhr.onload = function () {
     callback(this.responseText)
   };
-  xhr.open('GET', url, true);
+  xhr.open('GET', setting.api_url + url, true);
   xhr.send();
 }
 
---
+++ src/js/setting.js
@@ -0,0 +1,3 @@
+export var setting ={
+    api_url: 'http://127.0.0.1:8000'
+}
