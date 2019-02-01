f512aa9ee5febf1b1f4cc57f9fe3e0f3fa6992ff
skywalker512 <houzhenhong@hotmail.com>
2019-02-01 21:53:27 +0800

    [提交] 添加 下滑刷新的节流
    #5

--- src/tpl/index/article_list_controller.js
+++ src/tpl/index/article_list_controller.js
@@ -1,5 +1,6 @@
 import articleListApi from './article_list_api';
 import { tipsEle } from './index_body'
+import throttle from '../../js/throttle'
 
 export default function articleListController(ele) {
     var page = 1;
@@ -10,16 +11,14 @@ export default function articleListController(ele) {
     articleListApi(page, ele);
     var clickEle = document.createElement('div');
     clickEle.classList.add('buttom');
-    window.onscroll = function() {
+    window.addEventListener('scroll', throttle(function () {
         // 有问题 应该 需要预留位置
         if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight)) {
             tipsEle.classList.remove('hide');
-            setTimeout(()=>{
-                page++;
-                articleListApi(page, ele, hideTips);
-            }, 1000);
+            page++;
+            articleListApi(page, ele, hideTips);
         }
-    };
+    }, 500, 1000), false);
     clickEle.addEventListener('click', () => {
         page++;
         articleListApi(page, ele);
