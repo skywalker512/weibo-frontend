290fc7d3a19f95935f54b5fd67874aa3a99b1b3a
skywalker512 <houzhenhong@hotmail.com>
2019-02-03 10:23:24 +0800

    [提交] 文章列表 用户信息样式
    #5

--- src/tpl/dynamic/index/articleList.js
+++ src/tpl/dynamic/index/articleList.js
@@ -12,12 +12,10 @@ export default function articleList(page, ele, callback) {
                 <div class="a_text" href="article/{%=agrs[0][i].id%}">
                     <div class="text">{%= agrs[0][i].content %}</div>
                     <div class="info">
-                        <div class="user">
-                            {% const md5_mail = agrs[1](agrs[0][i].owner.belong_to.email) %} 
-                            <div class="avatar"><img src="https://cdn.v2ex.com/gravatar/{%= md5_mail %}?d=retro&s=64"></div>
-                            <div class="name">{%= agrs[0][i].owner.belong_to.username %}</div>
-                        </div>
-                        <div class="time">{%= agrs[0][i].add_date %}</div>
+                        {% const md5_mail = agrs[1](agrs[0][i].owner.belong_to.email) %} 
+                        <span class="avatar"><img src="https://cdn.v2ex.com/gravatar/{%= md5_mail %}?d=retro&s=64"></span>
+                        <span class="name">{%= agrs[0][i].owner.belong_to.username %}</span>
+                        <span class="time">{%= agrs[0][i].add_date %}</span>
                     </div>
                 </div>
             </div>
