87b5f6146306e0f8c89e21f7958275e6389bb2e9
skywalker512 <houzhenhong@hotmail.com>
2019-02-12 21:55:26 +0800

    [修改] 注册验证逻辑

--- controllers/account/loginbox.js
+++ controllers/account/loginbox.js
@@ -16,15 +16,33 @@ export default function loginController(body, loginEle) {
     { // 用户登陆信息判断
         const usernameInput = document.querySelector('#username');
         const passwordInput = document.querySelector('#password');
-        let mark = 0;
         buttonEle.addEventListener('click', () => {
-            if( !config.namePattern.test(usernameInput.value) && mark === 0) {
-                usernameInput.insertAdjacentHTML('beforebegin', `<div class="tips">用户名必须大于4个字符小于16个字符<span class="icon icon-index-close"></span></div>`);
-                mark = 1;
-            } else if( !config.namePattern.test(passwordInput.value) && mark === 0) {
-                passwordInput.insertAdjacentHTML('beforebegin', `<div class="tips">密码必须分别包含2个大小写字母,并且大于6个字符小于16个字符<span class="icon icon-index-close"></span></div>`);
-                mark = 1;
-            } else {
+            let tipsName = document.querySelector('.account .tips.name')
+            let tipsPassword = document.querySelector('.account .tips.password')
+            const isName = config.namePattern.test(usernameInput.value)
+            const isPassword = config.namePattern.test(passwordInput.value)
+
+            if( !isName && !tipsName ) {
+                usernameInput.insertAdjacentHTML('beforebegin', `<div class="tips name">用户名必须大于4个字符小于16个字符<span class="icon icon-index-close"></span></div>`);
+                tipsName = document.querySelector('.account .tips.name')
+                tipsName.addEventListener('click', ()=>{
+                    tipsName.parentNode.removeChild(tipsName);
+                })
+            } else if (isName && tipsName) {
+                tipsName.remove()
+            }
+
+            if( !isPassword && !tipsPassword ) {
+                passwordInput.insertAdjacentHTML('beforebegin', `<div class="tips password">密码必须分别包含2个大小写字母,并且大于6个字符小于16个字符<span class="icon icon-index-close"></span></div>`);
+                tipsPassword = document.querySelector('.account .tips.password')
+                tipsPassword.addEventListener('click', ()=>{
+                    tipsPassword.parentNode.removeChild(tipsPassword);
+                })
+            } else if ( isPassword && tipsPassword ) {
+                tipsPassword.remove()
+            }
+
+            if ( isName && isPassword ) {
                 ajax('POST', '/api/login', { info: usernameInput.value , password: passwordInput.value  }).then(result=>{
                     if( result.code === 200 ){
                         body.removeChild(loginEle);
@@ -34,15 +52,6 @@ export default function loginController(body, loginEle) {
                         userNav(result.data)
                     }
                 })
-
-            };
-
-            if (mark === 1) {
-                const tipsCloseEle = document.querySelector('.account .tips .icon-index-close');
-                tipsCloseEle.addEventListener('click', () => {
-                    tipsCloseEle.parentNode.parentNode.removeChild(tipsCloseEle.parentNode);
-                    mark = 0;
-                });
             };
         });
     };
