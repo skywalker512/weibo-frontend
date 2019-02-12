fe2864f9e3a4b3c94b9c0979ffaaaefc1064d9af
skywalker512 <houzhenhong@hotmail.com>
2019-02-12 19:42:00 +0800

    [提交] 发布框

--- view/article/postBox.js
+++ view/article/postBox.js
@@ -6,7 +6,25 @@ export default function() {
         <div class="outer"></div>
         <div class="inner">
             <div class="container">
-                <div class="close"><span class="icon icon-index-close"></span></div>
+                <div class="close"><a href="/"><span class="icon icon-index-close"></span></a></div>
+                <div class="title">有什么新鲜事想告诉大家?</div>
+                <div class="box">
+                    <div class="count">已输入<span id="count"></span>字</div>
+                    <div class="input">
+                        <textarea></textarea>
+                    </div>
+                    <div class="controller">
+                        <div class="function">                    
+                            <span class="icon icon-article-emoji"></span>
+                            <span class="icon icon-article-pic"></span>
+                            <span class="icon icon-article-video"></span>
+                            <span class="icon icon-aticle-topic"></span>
+                        </div>
+                        <div class="push">
+                            <button type="submit">发布</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         
