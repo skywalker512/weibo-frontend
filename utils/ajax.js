ba24f93fb478a58001a9fe2905e488a0f2cdd634
skywalker512 <houzhenhong@hotmail.com>
2019-02-12 12:06:50 +0800

    [提交] ajax 的 post 以及 跨域 问题

--- utils/ajax.js
+++ utils/ajax.js
@@ -1,7 +1,13 @@
-import {config} from '../config';
+import { config } from '../config';
 
 export default async function ajax(type, url, data) {
-    const response = await fetch(config.apiUrl+url, { method: type, body: JSON.stringify(data) }); // awit 返回的就是 Promise.resolve() 也就是 .then 传入的参数
+    let params;
+    if (data) {
+        params = Object.keys(data).map((key) => {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
+        }).join('&');
+    }
+    const response = await fetch(config.apiUrl + url, { method: type, credentials: 'include', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: params }); // awit 返回的就是 Promise.resolve() 也就是 .then 传入的参数
     const result = await response.json(); // 同样 awit 返回也是 then 中的参数
-    return result.data;
+    return result
 }
