5ad1e3a6863445411233239938e586a152186879
skywalker512 <houzhenhong@hotmail.com>
2019-02-13 21:11:44 +0800

    [提交] 发帖验证

--- controllers/article/postBox.js
+++ controllers/article/postBox.js
@@ -15,9 +15,17 @@ export default function(body, postBoxEle) {
         const pushBotton = document.querySelector('.post .push button')
         pushBotton.addEventListener('click', () =>{
             const textarea =  document.querySelector('.post textarea')
-            if( !config.articlePattern.test(textarea.value) ) {
-                console.log(1)
-            } else {
+            const isText = config.articlePattern.test(textarea.value)
+            let isTips = document.querySelector('.post .tips .icon')
+            if( !isTips && !isText ) {
+                const post = document.querySelector('.post .input')
+                post.insertAdjacentHTML('afterbegin', `<div class="tips">请不要输入特殊字符，且大于10个字符小于140个字符<span class="icon icon-index-close"></span></div>`)
+                isTips = document.querySelector('.post .tips .icon')
+                isTips.addEventListener('click', ()=>{
+                    isTips.parentNode.remove()
+                })
+            }
+            if(isText){
                 ajax('POST', '/api/article', { content: textarea.value , categoryId: '5c600f1c3fae496320e819e0'  }).then(result=>{
                     if( result.code === 200 ){
                         body.removeChild(postBoxEle)
