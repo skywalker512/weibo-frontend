f8fd001cfdffd49157911a2f33ba432feedd5a04
skywalker512 <houzhenhong@hotmail.com>
2019-02-13 22:21:31 +0800

    [提交]首页列表添加 点赞和评论标签

--- api/indexBody/article.js
+++ api/indexBody/article.js
@@ -9,10 +9,19 @@ export default function article(article) {
                 <div class="a_text" href="article/{%=agrs[0][i]._id%}">
                     <div class="text">{%= agrs[0][i].content %}</div>
                     <div class="info">
-                        <span class="avatar"><img src={%= agrs[0][i].authorId.avatar%}></span>
-                        <span class="name">{%= agrs[0][i].authorId.name %}</span>
-                        {% const time = new Date(agrs[0][i].updatedAt) %}
-                        <span class="time">{%= time.toLocaleString() %}</span>
+                        <div class="left">
+                            <span class="avatar"><img src={%= agrs[0][i].authorId.avatar%}></span>
+                            <span class="name">{%= agrs[0][i].authorId.name %}</span>
+                            {% const time = new Date(agrs[0][i].updatedAt) %}
+                            <span class="time">{%= time.toLocaleString() %}</span>
+                        </div>
+                        <div class="right">
+                            <span class="icon icon-pinglun"></span>
+                            <span class="text pinglun">{%= agrs[0][i].commentNum %}</span>
+                            <span class="grey-line"></span>                            
+                            <span class="icon icon-zan"></span>
+                            <span class="text zan">{%= agrs[0][i].praiseNum %}</span>
+                        </div>
                     </div>
                 </div>
             </div>
