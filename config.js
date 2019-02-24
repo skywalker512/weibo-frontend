b22ada5e361bc0273589adced8786197093769a7
skywalker512 <houzhenhong@hotmail.com>
2019-02-24 12:17:25 +0800

    [修改] 修改本地测试 url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    apiUrl: 'https://api.redrock.atowerlight.cn',
+    apiUrl: 'http://redrock.test:3000',
     passwordPattern: /^.*(?=.{4,16})(?=.*\d)(?=.*[A-Z]{2,})(?=.*[a-z]{2,}).*$/,
     emailPattern: /^.*(?=.{1,30})(?=.*[@]).*$/,
     namePattern: /^[\w_-]{6,16}$/,
