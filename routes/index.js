589566f40139dda8ef490d5f9bad26ca6e4c89a4
skywalker512 <houzhenhong@hotmail.com>
2019-02-25 12:50:16 +0800

    [修改] 记录 dom 变化的时间

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ export default function () {
 
     const config = { attributes: false, childList: true, subtree: true };
     const callback = records => {
-        router.proxyLinks(records[records.length - 1].target.querySelectorAll('a')) // 记录最后一个，以防触到浏览器限制
+        router.proxyLinks(records[0].target.querySelectorAll('a')) // 记录第一个，以防触到浏览器限制
     }
     const observer = new MutationObserver(callback)
     observer.observe(document.querySelector('body'), config);
