897c0c9e5206539082811f0ad17821dec5681477
skywalker512 <houzhenhong@hotmail.com>
2019-02-26 09:12:59 +0800

    [提交] 搜索高亮关键词

--- api/searchBox.js
+++ api/searchBox.js
@@ -2,7 +2,7 @@ import ajax from '../utils/ajax'
 import article from './indexBody/article'
 
 export async function searchApi(value) {
-    await ajax('GET', '/api/search/'+ value ).then(result=>{
+    await ajax('GET', '/api/search/'+encodeURIComponent(value) ).then(result=>{
         article(result.data, false, '.search-article');
     })
 }
--- controllers/search/searchBox.js
+++ controllers/search/searchBox.js
@@ -14,4 +14,13 @@ export default async function (searchBoxEle, value) {
             document.body.classList.remove('body-fixed')
         });
     }
+
+    { // 高亮关键词
+        const tests = searchBoxEle.querySelectorAll('.text-box .text')
+        for(let i=0; i < tests.length; i++) {
+            const values = tests[i].textContent.split(value)
+            tests[i].innerHTML = values.join('<span class="search-light">' + value + '</span>');
+        }
+
+    }
 }
