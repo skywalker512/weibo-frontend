0958776ffe79f2dd1a94a6ed9e16f8d1815846a0
skywalker512 <houzhenhong@hotmail.com>
2019-03-02 08:59:22 +0800

    [提交] 实现回车搜索

--- controllers/search/searchBottom.js
+++ controllers/search/searchBottom.js
@@ -1,18 +1,29 @@
 export default function () {
     const sreachBottom = document.querySelector('.header .search .icon.icon-nav-sreach')
-    sreachBottom.addEventListener('click', ()=>{
-        const input = document.querySelector('.header .search input')
-        if (input.value.length > 0) {
-            import(/* webpackChunkName: "searchbox" */ '../../less/search/searchBox.less');
-            import(/* webpackChunkName: "searchbox" */ '../../view/search/searchBox').then(module => {
-                const searchBox = module.default;
-                const searchBoxEle = searchBox();
-                document.body.appendChild(searchBoxEle);
-                import(/* webpackChunkName: "searchbox" */ './searchBox').then(module => {
-                    const searchBoxController = module.default;
-                    searchBoxController(searchBoxEle, input.value)
-                })
+    const input = document.querySelector('.header .search input')
+
+    function popup() {
+        import(/* webpackChunkName: "searchbox" */ '../../less/search/searchBox.less');
+        import(/* webpackChunkName: "searchbox" */ '../../view/search/searchBox').then(module => {
+            const searchBox = module.default;
+            const searchBoxEle = searchBox();
+            document.body.appendChild(searchBoxEle);
+            import(/* webpackChunkName: "searchbox" */ './searchBox').then(module => {
+                const searchBoxController = module.default;
+                searchBoxController(searchBoxEle, input.value)
             })
+        })
+    }
+
+    sreachBottom.addEventListener('click', () => {
+        if (input.value.length > 0) {
+            popup()
+        }
+    })
+
+    input.addEventListener('keyup', (e)=>{
+        if(e.keyCode === 13 && input.value.length > 0) {
+            popup()
         }
     })
 }
